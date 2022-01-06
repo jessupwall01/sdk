0cd694d80b44e6bff2c5df52928ef45f4f6e4f27
Manank Patni <manank321@gmail.com>
2022-01-07 04:19:03 +0530

    Add fixes

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  BSCTEST = 97
 }
 
 export enum TradeType {
@@ -22,9 +23,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xA481a5D67ED8071F4AE36136fD2E97f601cEe5A8'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0xb802e697b92f5e5458ba9c9b95ba6ed88c10e9b59edf725d1112fbbe04896784'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
